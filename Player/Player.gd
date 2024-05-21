3a22bd6318909eb993ddbdf07eff497c4f4861d3
合金装备草履虫 <jh2194219463@163.com>
2024-05-21 16:54:24 +0800

    p2中
    完成了惯性刹车，但具体参数没搞懂，而且惯性开始没实现，有点不理解

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 #4.0后，velocity在2D中被定义，无需重复定义
+const MAX_SPEED = 100
 
 func _ready():
 	print("start")
@@ -9,12 +10,15 @@ func _physics_process(delta):
 	
 	input_vector.x = Input.get_action_strength("ui_right")-Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down")-Input.get_action_strength("ui_up")
+	#方向向量单位化
+	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
-		velocity = input_vector
+		velocity = input_vector * MAX_SPEED
+		print(velocity)
 	else:
-		velocity = Vector2.ZERO
+		velocity = velocity.move_toward(Vector2.ZERO , 5)
 		
 	#传递速度
-	move_and_collide(velocity)
+	move_and_collide(velocity * delta)
 	
