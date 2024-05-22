8176f50eb276d796f469969bf5afb4a249c559a1
合金装备草履虫 <jh2194219463@163.com>
2024-05-22 11:04:38 +0800

    p2学完，优化了代码

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,15 +1,17 @@
 extends CharacterBody2D
 
 #4.0后，velocity在2D中被定义，无需重复定义
-const MAX_SPEED = 200
+const MAX_SPEED = 5
 #加速度
-const ACCELEARTION = 25
+const ACCELEARTION = 20
 #摩擦力
-const FRNCTION = 25
+const FRNCTION = 50
 
 func _ready():
 	print("start")
+#delta是上一帧的运行时间
 func _physics_process(delta):
+	#vector 方向向量 包含了x,y的单位量
 	var input_vector = Vector2.ZERO
 	
 	input_vector.x = Input.get_action_strength("ui_right")-Input.get_action_strength("ui_left")
@@ -18,14 +20,13 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
-		velocity += input_vector * ACCELEARTION * delta
-		velocity = velocity.limit_length(MAX_SPEED * delta)
-		print(velocity)
+		#move_toward : 目标速度向量，每秒变化，摩擦力的加速度
+		velocity = velocity.move_toward(input_vector*MAX_SPEED , ACCELEARTION*delta)
 	else:
-		#move_toward : 目标速度，每秒变化
 		velocity = velocity.move_toward(Vector2.ZERO , FRNCTION*delta)
 		
 	#传递速度
 	move_and_collide(velocity)
+	print(velocity)
 	
 	
