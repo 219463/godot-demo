c4da0857ba271cf4600b2b5d49d16bf0114f2443
合金装备草履虫 <jh2194219463@163.com>
2024-05-27 14:54:35 +0800

    p14完成

--- Enemies/bat.gd
+++ Enemies/bat.gd
@@ -1,5 +1,9 @@
 extends CharacterBody2D
 
+@onready var stats = $Stats
+
+
+
 func  _physics_process(delta):
 	velocity = velocity.move_toward(Vector2.ZERO,300*delta)
 	move_and_slide()
@@ -7,3 +11,8 @@ func  _physics_process(delta):
 func _on_hurtbox_area_entered(area):
 	var vector = (global_position - area.global_position).normalized()
 	velocity = vector * 100
+	stats.health-=area.damage
+
+
+func _on_stats_dead():
+	queue_free()
---
+++ Stats.gd
@@ -0,0 +1,11 @@
+extends Node
+
+@export var max_health:int = 1
+@onready var health = max_health : set = set_health
+
+signal dead
+#每次health被改变时调用
+func set_health(value):
+	health = value
+	if health <= 0:
+		emit_signal("dead")
